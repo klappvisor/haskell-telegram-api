0099624356001142a6997eef8289b52ea975d80f
Alexey Rodiontsev <alex.rodiontsev@gmail.com>
2016-01-02 11:23:18 +0400

    getMe implementation is done

--- app/Main.hs
+++ app/Main.hs
@@ -1,31 +1,36 @@
-{-# LANGUAGE DataKinds       #-}
-{-# LANGUAGE TemplateHaskell #-}
-{-# LANGUAGE TypeOperators   #-}
+{-# LANGUAGE DataKinds         #-}
+{-# LANGUAGE DeriveGeneric     #-}
+{-# LANGUAGE DeriveAnyClass    #-}
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TypeOperators     #-}
+{-# LANGUAGE TemplateHaskell   #-}
 
 module Main where
 
+import Control.Monad
 import Telegram.API.Bots
 import Servant
+import Data.Text (Text)
+import qualified Data.Text.IO as T
 import Network.Wai
 import Network.Wai.Handler.Warp
 
 main :: IO ()
-main = startApp
+main = do
+  Right GetMeResponse { result = u } <- getMe (Token "bot179176211:AAGtOVb_YxcpK8FaJ-ixpdOKsEEy-7LINH0")
+  T.putStrLn (first_name u)
+--main = startApp
 
-startApp :: IO ()
-startApp = run 8080 app
+--startApp :: IO ()
+--startApp = run 8080 app
 
-app :: Application
-app = serve api server
-
-api :: Proxy API
-api = Proxy
-
-server :: Server API
-server = return users
-
-users :: [User]
-users = [ User 1 "Test" (Just "Test") Nothing
-        , User 2 "TEST" (Just "TEST") Nothing
-        ]
+--app :: Application
+--app = serve api server
+--
+--server :: Server API
+--server = return users
+--
+--users :: [User]
+--users = [ User 1 "Test" (Just "Test") Nothing
+--        , User 2 "TEST" (Just "TEST") Nothing
+--        ]
--- src/Telegram/API/Bots.hs
+++ src/Telegram/API/Bots.hs
@@ -3,39 +3,57 @@
 {-# LANGUAGE DeriveAnyClass    #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE TypeOperators     #-}
-{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE TemplateHaskell   #-}
 
-module Telegram.API.Bots where
+module Telegram.API.Bots
+  (
+    getMe
+  , TelegramBotsAPI
+  , Token             (..)
+  , User              (..)
+  , GetMeResponse     (..)
+  ) where
 
-import Data.Aeson
-import Data.Aeson.TH
-import Servant.API
-import Servant.Client
-import Data.Text (Text)
-import GHC.Generics (Generic)
+import           Control.Applicative
+import           Control.Monad.Trans.Either
+import           Data.Aeson
+import           Data.Aeson.Types
+import           Data.Maybe
+import           Data.Proxy
+import           Data.Text (Text)
+import qualified Data.Text as T
+import           GHC.Generics
+import           GHC.TypeLits
+import           Servant.API
+import           Servant.Client
 
 newtype Token = Token Text
-  deriving (ToText, FromText, Show, Eq, Ord)
+  deriving (Show, Eq, Ord)
+
+instance ToText Token where
+  toText (Token x) = x
+
+instance FromText Token where
+  fromText x =Just (Token (x))
 
 data User = User {
     id :: Int
   , first_name :: Text
   , last_name :: Maybe (Text)
   , username :: Maybe (Text)
-  } deriving (Show, Generic)
+  } deriving (FromJSON, ToJSON, Show, Generic)
 
 data GetMeResponse = GetMeResponse {
     result :: User
-  } deriving (Show, Eq, Ord, Generic)
+  } deriving (FromJSON, ToJSON, Show, Generic)
 
-instance FromJSON GetMeResponse
+type TelegramBotsAPI = Capture ":token" Token :> "getMe" :> Get '[JSON] GetMeResponse
 
-type TelegramBotsAPI = "users" :> Get '[JSON] [User]
+api :: Proxy TelegramBotsAPI
+api = Proxy
 
 getMe' :: Token -> EitherT ServantError IO GetMeResponse
-getMe' token = client api (BaseUrl Https ("api.telegram.org/bot" <> token) 443)
+getMe' = client api (BaseUrl Https "api.telegram.org" 443)
 
 getMe :: Token -> IO (Either ServantError GetMeResponse)
 getMe token = runEitherT $ getMe' token
-
-
