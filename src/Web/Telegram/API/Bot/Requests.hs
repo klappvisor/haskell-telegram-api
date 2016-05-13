e42c0d510e4a08053a2ed201b7f1a1e78b4b9f06
Arguggi <git@arguggi.co.uk>
2016-05-13 11:36:01 +0200

    Add fields to AnswerInlineQueryRequest

--- src/Web/Telegram/API/Bot/Requests.hs
+++ src/Web/Telegram/API/Bot/Requests.hs
@@ -285,6 +285,15 @@ data AnswerInlineQueryRequest = AnswerInlineQueryRequest
   , query_cache_time      :: Maybe Int -- ^ The maximum amount of time in seconds that the result of the inline query may be cached on the server. Defaults to 300.
   , query_is_personal     :: Maybe Bool -- ^ Pass True, if results may be cached on the server side only for the user that sent the query. By default, results may be returned to any user who sends the same query
   , query_next_offset     :: Maybe Text -- ^ Pass the offset that a client should send in the next query with the same text to receive more results. Pass an empty string if there are no more results or if you don‘t support pagination. Offset length can’t exceed 64 bytes.
+  , query_switch_pm_text  :: Maybe Text -- ^ If passed, clients will display a button with specified text that switches the user to a private chat with the bot and sends the bot a start message with the parameter switch_pm_parameter
+  , query_switch_pm_parameter :: Maybe Text -- ^ Parameter for the start message sent to the bot when user presses the switch button
+                                            --
+                                            -- Example: An inline bot that sends YouTube videos can ask the user to connect the bot to 
+                                            -- their YouTube account to adapt search results accordingly. To do this, it displays a 
+                                            -- ‘Connect your YouTube account’ button above the results, or even before showing any. 
+                                            -- The user presses the button, switches to a private chat with the bot and, in doing so, 
+                                            -- passes a start parameter that instructs the bot to return an oauth link. Once done, 
+                                            -- the bot can offer a switch_inline button so that the user can easily return to the chat where they wanted to use the bot's inline capabilities.
   } deriving (Show, Generic)
 
 instance ToJSON AnswerInlineQueryRequest where
