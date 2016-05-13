be80bae2de48674d610f234b44ae8638725c7832
Arguggi <git@arguggi.co.uk>
2016-05-13 10:03:25 +0200

    Add missing fields in Requests
    Adds all the disable_notifications and a caption in SendPhotoRequest

--- test/MainSpec.hs
+++ test/MainSpec.hs
@@ -33,44 +33,44 @@ spec token chatId botName = do
 
   describe "/sendMessage" $ do
     it "should send message" $ do
-      res <- sendMessage token (SendMessageRequest chatId "test message" Nothing Nothing Nothing Nothing) manager
+      res <- sendMessage token (SendMessageRequest chatId "test message" Nothing Nothing Nothing Nothing Nothing) manager
       success res 
       let Right MessageResponse { message_result = m } = res
       (text m) `shouldBe` (Just "test message")
 
     it "should return error message" $ do
-      res <- sendMessage token (SendMessageRequest "" "test message" Nothing Nothing Nothing Nothing) manager
+      res <- sendMessage token (SendMessageRequest "" "test message" Nothing Nothing Nothing Nothing Nothing) manager
       nosuccess res
       let Left FailureResponse { responseStatus = Status { statusMessage = msg } } = res
       msg `shouldBe` "Bad Request"
 
     it "should send message markdown" $ do
-      res <- sendMessage token (SendMessageRequest chatId "text *bold* _italic_ [github](github.com/klappvisor/telegram-api)" (Just Markdown) Nothing Nothing Nothing) manager
+      res <- sendMessage token (SendMessageRequest chatId "text *bold* _italic_ [github](github.com/klappvisor/telegram-api)" (Just Markdown) Nothing Nothing Nothing Nothing) manager
       success res
       let Right MessageResponse { message_result = m } = res
       (text m) `shouldBe` (Just "text bold italic github")
 
     it "should set keyboard" $ do
-      res <- sendMessage token (SendMessageRequest chatId "set keyboard" Nothing Nothing Nothing (Just (ReplyKeyboardMarkup [["A", "B"], ["C"]] Nothing Nothing Nothing))) manager
+      res <- sendMessage token (SendMessageRequest chatId "set keyboard" Nothing Nothing Nothing Nothing (Just (ReplyKeyboardMarkup [["A", "B"], ["C"]] Nothing Nothing Nothing))) manager
       success res
       let Right MessageResponse { message_result = m } = res
       (text m) `shouldBe` (Just "set keyboard")
 
     it "should remove keyboard" $ do
-      res <- sendMessage token (SendMessageRequest chatId "remove keyboard" Nothing Nothing Nothing (Just (ReplyKeyboardHide True Nothing))) manager
+      res <- sendMessage token (SendMessageRequest chatId "remove keyboard" Nothing Nothing Nothing Nothing (Just (ReplyKeyboardHide True Nothing))) manager
       success res
       let Right MessageResponse { message_result = m } = res
       (text m) `shouldBe` (Just "remove keyboard")
 
     it "should force reply" $ do
-      res <- sendMessage token (SendMessageRequest chatId "force reply" Nothing Nothing Nothing (Just (ForceReply True Nothing))) manager
+      res <- sendMessage token (SendMessageRequest chatId "force reply" Nothing Nothing Nothing Nothing (Just (ForceReply True Nothing))) manager
       success res
       let Right MessageResponse { message_result = m } = res
       (text m) `shouldBe` (Just "force reply")
 
   describe "/forwardMessage" $ do
     it "should forward message" $ do
-      res <- forwardMessage token (ForwardMessageRequest chatId chatId 123) manager
+      res <- forwardMessage token (ForwardMessageRequest chatId chatId Nothing 123) manager
       nosuccess res
       let Left FailureResponse { responseStatus = Status { statusMessage = msg } } = res
       msg `shouldBe` "Bad Request"
@@ -78,47 +78,47 @@ spec token chatId botName = do
   describe "/sendPhoto" $ do
     it "should return error message" $ do
       Left FailureResponse { responseStatus = Status { statusMessage = msg } } <-
-        sendPhoto token (SendPhotoRequest "" "photo_id" (Just "photo caption") Nothing Nothing) manager
+        sendPhoto token (SendPhotoRequest "" "photo_id" (Just "photo caption") Nothing Nothing Nothing) manager
       msg `shouldBe` "Bad Request"
     it "should send photo" $ do
      Right MessageResponse { message_result = Message { caption = Just cpt } } <-
-       sendPhoto token (SendPhotoRequest chatId catPic (Just "photo caption") Nothing Nothing) manager
+       sendPhoto token (SendPhotoRequest chatId catPic (Just "photo caption") Nothing Nothing Nothing) manager
      cpt `shouldBe` "photo caption"
 
   describe "/sendAudio" $ do
     it "should return error message" $ do
       Left FailureResponse { responseStatus = Status { statusMessage = msg } } <-
-        sendAudio token (SendAudioRequest "" "audio_id" Nothing (Just "performer") (Just "title") Nothing Nothing) manager
+        sendAudio token (SendAudioRequest "" "audio_id" Nothing (Just "performer") (Just "title") Nothing Nothing Nothing) manager
       msg `shouldBe` "Bad Request"
     it "should send audio" $ do
       Right MessageResponse { message_result = Message { audio = Just Audio { audio_title = Just title } } } <-
-        sendAudio token (SendAudioRequest chatId "BQADBAADAQQAAiBOnQHThzc4cz1-IwI" Nothing Nothing Nothing Nothing Nothing) manager
+        sendAudio token (SendAudioRequest chatId "BQADBAADAQQAAiBOnQHThzc4cz1-IwI" Nothing Nothing Nothing Nothing Nothing Nothing) manager
       title `shouldBe` "The Nutcracker Suite - Act II, No.12. Pas de Deux variations"
 
   describe "/sendSticker" $ do
     it "should send sticker" $ do
       Right MessageResponse { message_result = Message { sticker = Just sticker } } <-
-        sendSticker token (SendStickerRequest chatId "BQADAgADGgADkWgMAAGXlYGBiM_d2wI" Nothing Nothing) manager
+        sendSticker token (SendStickerRequest chatId "BQADAgADGgADkWgMAAGXlYGBiM_d2wI" Nothing Nothing Nothing) manager
       (sticker_file_id sticker) `shouldBe` "BQADAgADGgADkWgMAAGXlYGBiM_d2wI"
 
   describe "/sendLocation" $ do
     it "should send location" $ do
       Right MessageResponse { message_result = Message { location = Just loc } } <-
-        sendLocation token (SendLocationRequest chatId 52.38 4.9 Nothing Nothing) manager
+        sendLocation token (SendLocationRequest chatId 52.38 4.9 Nothing Nothing Nothing) manager
       (latitude loc) `shouldSatisfy` (liftM2 (&&) (> 52) (< 52.4))
       (longitude loc) `shouldSatisfy` (liftM2 (&&) (> 4.89) (< 5))
 
   describe "/sendVenue" $ do
     it "should send a venue" $ do
       Right MessageResponse { message_result = Message { location = Just loc } } <-
-        sendVenue token (SendVenueRequest chatId 52.38 4.9 "Amsterdam Centraal" "Amsterdam" Nothing Nothing Nothing) manager
+        sendVenue token (SendVenueRequest chatId 52.38 4.9 "Amsterdam Centraal" "Amsterdam" Nothing Nothing Nothing Nothing) manager
       (latitude loc) `shouldSatisfy` (liftM2 (&&) (> 52) (< 52.4))
       (longitude loc) `shouldSatisfy` (liftM2 (&&) (> 4.89) (< 5))
 
   describe "/sendContact" $ do
     it "should send a contact" $ do
       Right MessageResponse { message_result = Message { contact = Just con } } <-
-        sendContact token (SendContactRequest chatId "06-18035176" "Hilbert" Nothing Nothing Nothing) manager
+        sendContact token (SendContactRequest chatId "06-18035176" "Hilbert" Nothing Nothing Nothing Nothing) manager
       -- Telegram seems to remove any non numeric characters from the sent phone number (at least it removed my '-')
       (contact_phone_number con) `shouldBe` "0618035176"
       (contact_first_name con) `shouldBe` "Hilbert"
@@ -173,7 +173,7 @@ spec token chatId botName = do
 
   describe "/editTextMessage" $ do
     it "should edit message" $ do
-      let originalMessage = SendMessageRequest chatId "veritas" Nothing Nothing Nothing Nothing
+      let originalMessage = SendMessageRequest chatId "veritas" Nothing Nothing Nothing Nothing Nothing
       Right MessageResponse { message_result = Message { message_id = msg_id, text = Just txt } } <-
         sendMessage token originalMessage manager
       Right MessageResponse { message_result = Message { text = txt' } } <-
@@ -181,7 +181,7 @@ spec token chatId botName = do
       txt' `shouldBe` Just "edited veritas"
 
     it "should edit caption" $ do
-      let originalMessage = SendPhotoRequest chatId catPic (Just "cat picture") Nothing Nothing
+      let originalMessage = SendPhotoRequest chatId catPic (Just "cat picture") Nothing Nothing Nothing
       Right MessageResponse { message_result = Message { message_id = msg_id, caption = Just cpt } } <-
         sendPhoto token originalMessage manager
       Right MessageResponse { message_result = Message { caption = Just cpt' } } <-
