13ef5b85411e11b53ea18f11f0e19fb9055b68ec
Alexey Rodiontsev <alex.rodiontsev@gmail.com>
2016-05-22 10:26:56 +0200

    added methods for administration

--- src/Web/Telegram/API/Bot/API.hs
+++ src/Web/Telegram/API/Bot/API.hs
@@ -31,7 +31,12 @@ module Web.Telegram.API.Bot.API
   , answerInlineQuery
   , answerCallbackQuery
   , kickChatMember
+  , leaveChat
   , unbanChatMember
+  , getChat
+  , getChatAdministrators
+  , getChatMembersCount
+  , getChatMember
   , editMessageText
   , editMessageCaption
   , editMessageReplyMarkup
@@ -147,10 +152,23 @@ type TelegramBotAPI =
          :> QueryParam "chat_id" Text
          :> QueryParam "user_id" Int
          :> Post '[JSON] KickChatMemberResponse
+    :<|> TelegramToken :> "leaveChat"
+         :> QueryParam "chat_id" Text
+         :> Post '[JSON] LeaveChatResponse
     :<|> TelegramToken :> "unbanChatMember"
          :> QueryParam "chat_id" Text
          :> QueryParam "user_id" Int
          :> Post '[JSON] UnbanChatMemberResponse
+    :<|> TelegramToken :> "getChat"
+         :> QueryParam "chat_id" Text
+         :> Post '[JSON] GetChatResponse
+    :<|> TelegramToken :> "getChatAdministrators"
+         :> QueryParam "chat_id" Text
+         :> Post '[JSON] GetChatAdministratorsResponse
+    :<|> TelegramToken :> "getChatMember"
+         :> QueryParam "chat_id" Text
+         :> QueryParam "user_id" Int
+         :> Post '[JSON] GetChatMemberResponse
     :<|> TelegramToken :> "editMessageText"
          :> ReqBody '[JSON] EditMessageTextRequest
          :> Post '[JSON] MessageResponse
@@ -192,7 +210,12 @@ setWebhook_                :: Token -> Maybe Text -> Manager -> BaseUrl -> Excep
 answerInlineQuery_         :: Token -> AnswerInlineQueryRequest -> Manager -> BaseUrl -> ExceptT ServantError IO InlineQueryResponse
 answerCallbackQuery_       :: Token -> AnswerCallbackQueryRequest -> Manager -> BaseUrl -> ExceptT ServantError IO CallbackQueryResponse
 kickChatMember_            :: Token -> Maybe Text -> Maybe Int -> Manager -> BaseUrl -> ExceptT ServantError IO KickChatMemberResponse
+leaveChat_                 :: Token -> Maybe Text -> Manager -> BaseUrl -> ExceptT ServantError IO LeaveChatResponse
 unbanChatMember_           :: Token -> Maybe Text -> Maybe Int -> Manager -> BaseUrl -> ExceptT ServantError IO UnbanChatMemberResponse
+getChat_                   :: Token -> Maybe Text -> Manager -> BaseUrl -> ExceptT ServantError IO GetChatResponse
+getChatAdministrators_     :: Token -> Maybe Text -> Manager -> BaseUrl -> ExceptT ServantError IO GetChatAdministratorsResponse
+getChatMembersCount_       :: Token -> Maybe Text -> Manager -> BaseUrl -> ExceptT ServantError IO GetChatMembersCountResponse
+getChatMember_             :: Token -> Maybe Text -> Maybe Int -> Manager -> BaseUrl -> ExceptT ServantError IO GetChatMemberResponse
 editMessageText_           :: Token -> EditMessageTextRequest -> Manager -> BaseUrl ->  ExceptT ServantError IO MessageResponse
 editMessageCaption_        :: Token -> EditMessageCaptionRequest -> Manager -> BaseUrl ->  ExceptT ServantError IO MessageResponse
 editMessageReplyMarkup_    :: Token -> EditMessageReplyMarkupRequest -> Manager -> BaseUrl ->  ExceptT ServantError IO MessageResponse
@@ -222,7 +245,13 @@ getMe_
   :<|> answerInlineQuery_
   :<|> answerCallbackQuery_
   :<|> kickChatMember_
+  :<|> leaveChat_
   :<|> unbanChatMember_
+  :<|> getChat_
+  :<|> getChatAdministrators_
+  :<|> getChatMembersCount_
+  :<|> getChatMembersCount_
+  :<|> getChatMember_
   :<|> editMessageText_
   :<|> editMessageCaption_
   :<|> editMessageReplyMarkup_ =
@@ -344,9 +373,29 @@ answerCallbackQuery = run telegramBaseUrl answerCallbackQuery_
 kickChatMember :: Token -> Text -> Int -> Manager -> IO (Either ServantError KickChatMemberResponse)
 kickChatMember token chat_id user_id manager = runExceptT $ kickChatMember_ token (Just chat_id) (Just user_id) manager telegramBaseUrl
 
+-- | Use this method for your bot to leave a group, supergroup or channel. Returns True on success.
+leaveChat :: Token -> Maybe Text -> Manager -> BaseUrl -> IO (Either ServantError LeaveChatResponse)
+leaveChat token chat_id manager = runExceptT $ leaveChat_ token (Just chat_i) manager telegramBaseUrl
+
 -- | Use this method to unban a previously kicked user in a supergroup. The user will not return to the group automatically, but will be able to join via link, etc. The bot must be an administrator in the group for this to work.
 unbanChatMember :: Token -> Text -> Int -> Manager -> IO (Either ServantError UnbanChatMemberResponse)
 unbanChatMember token chat_id user_id manager = runExceptT $ unbanChatMember_ token (Just chat_id) (Just user_id) manager telegramBaseUrl
+
+-- | Use this method to get up to date information about the chat (current name of the user for one-on-one conversations, current username of a user, group or channel, etc.)
+getChat :: Token -> Text -> Manager -> IO (Either ServantError GetChatResponse)
+getChat token chat_id manager = runExceptT $ getChat_ token (Just chat_i) manager telegramBaseUrl
+
+-- | Use this method to get a list of administrators in a chat. On success, returns an Array of ChatMember objects that contains information about all chat administrators except other bots. If the chat is a group or a supergroup and no administrators were appointed, only the creator will be returned.
+getChatAdministrators :: Token -> Text -> Manager -> IO (Either ServantError GetChatAdministratorsResponse)
+getChatAdministrators token chat_id manager = runExceptT $ getChatAdministrators_ token (Just chat_i) manager telegramBaseUrl
+
+-- | Use this method to get the number of members in a chat. Returns Int on success.
+getChatMembersCount :: Token -> Text -> Manager -> IO (Either ServantError GetChatMembersCountResponse)
+getChatMembersCount token chat_id manager = runExceptT $ getChatMembersCount_ token (Just chat_i) manager telegramBaseUrl
+
+-- | Use this method to get information about a member of a chat. Returns a ChatMember object on success.
+getChatMember :: Token -> Text -> Int -> Manager -> IO (Either ServantError GetChatMemberResponse)
+getChatMember token chat_id user_id manager = runExceptT $ getChatMember_ token (Just chat_i) (Just user_id) manager telegramBaseUrl
 
 -- | Use this method to edit text messages sent by the bot or via the bot (for inline bots). On success, if edited message is sent by the bot, the edited `Message` is returned, otherwise True is returned.
 editMessageText :: Token -> EditMessageTextRequest -> Manager -> IO (Either ServantError MessageResponse)
