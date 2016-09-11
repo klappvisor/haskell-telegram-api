0b0e29137b514b15ffe7fc4d0da32d9b15f160e5
Alexey Rodiontsev <alex.rodiontsev@gmail.com>
2016-09-11 21:41:20 +0200

    fixed aeson-1.0 dependency

--- src/Web/Telegram/API/Bot/JsonExt.hs
+++ src/Web/Telegram/API/Bot/JsonExt.hs
@@ -13,12 +13,12 @@ import           Data.Aeson.Types
 import           GHC.Generics
 
 -- | Method used to drop prefix from field name during serialization
-toJsonDrop :: forall a.(GHC.Generics.Generic a, GToJSON (GHC.Generics.Rep a)) => Int -> a -> Value
+toJsonDrop :: forall a.(Generic a, GToJSON Zero (Rep a)) => Int -> a -> Value
 toJsonDrop prefix = genericToJSON defaultOptions {
     fieldLabelModifier = drop prefix
   , omitNothingFields = True
   }
 
 -- | Method used to drop prefix from field name during deserialization
-parseJsonDrop :: forall a.(Generic a, GFromJSON (Rep a)) => Int -> Value -> Parser a
+parseJsonDrop :: forall a.(Generic a, GFromJSON Zero (Rep a)) => Int -> Value -> Parser a
 parseJsonDrop prefix = genericParseJSON defaultOptions { fieldLabelModifier = drop prefix }
