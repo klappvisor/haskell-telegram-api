d47de65d63cdeec5c0fb74df941782d58cbc5838
Yuriy Syrovetskiy <cblp@cblp.su>
2016-12-08 17:00:54 +0400

    add argument docs to `getUpdates`

--- src/Web/Telegram/API/Bot/API.hs
+++ src/Web/Telegram/API/Bot/API.hs
@@ -364,7 +364,13 @@ sendChatAction :: Token -> SendChatActionRequest -> Manager -> IO (Either Servan
 sendChatAction = run telegramBaseUrl sendChatAction_
 
 -- | Use this method to receive incoming updates using long polling. An Array of 'Update' objects is returned.
-getUpdates :: Token -> Maybe Int -> Maybe Int -> Maybe Int -> Manager -> IO (Either ServantError UpdatesResponse)
+getUpdates
+    :: Token
+    -> Maybe Int -- ^ offset
+    -> Maybe Int -- ^ limit
+    -> Maybe Int -- ^ timeout
+    -> Manager
+    -> IO (Either ServantError UpdatesResponse)
 getUpdates token offset limit timeout manager = runClientM (getUpdates_ token offset limit timeout) $ ClientEnv manager telegramBaseUrl
 
 -- | Use this method to get basic info about a file and prepare it for downloading. For the moment, bots can download files of up to 20MB in size. On success, a 'File' object is returned. The file can then be downloaded via the link @https://api.telegram.org/file/bot<token>/<file_path>@, where @<file_path>@ is taken from the response. It is guaranteed that the link will be valid for at least 1 hour. When the link expires, a new one can be requested by calling getFile again.
