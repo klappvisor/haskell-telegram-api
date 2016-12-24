e5b2e26147eed7b77ca2b9e2641bc00ddb55e85c
Alexey Rodiontsev <ingar.ne@gmail.com>
2016-12-24 21:45:30 +0100

    added remove_keyboard

--- src/Web/Telegram/API/Bot/Requests.hs
+++ src/Web/Telegram/API/Bot/Requests.hs
@@ -572,6 +572,11 @@ data ReplyKeyboard =
     reply_hide_keyboard        :: Bool -- ^ Requests clients to hide the custom keyboard
   , reply_selective            :: Maybe Bool -- ^ Use this parameter if you want to hide keyboard for specific users only. Targets: 1) users that are @mentioned in the text of the Message object; 2) if the bot's message is a reply (has reply_to_message_id), sender of the original message.    Example: A user votes in a poll, bot returns confirmation message in reply to the vote and hides keyboard for that user, while still showing the keyboard with poll options to users who haven't voted yet.
   }
+  | ReplyKeyboardRemove
+  {
+    reply_remove_keyboard      :: Bool -- ^ Requests clients to remove the custom keyboard (user will not be able to summon this keyboard; if you want to hide the keyboard from sight but keep it accessible, use one_time_keyboard in 'ReplyKeyboardMarkup')
+  , reply_selective            :: Maybe Bool -- ^ Use this parameter if you want to hide keyboard for specific users only. Targets: 1) users that are @mentioned in the text of the Message object; 2) if the bot's message is a reply (has reply_to_message_id), sender of the original message.    Example: A user votes in a poll, bot returns confirmation message in reply to the vote and hides keyboard for that user, while still showing the keyboard with poll options to users who haven't voted yet.
+  }
   -- | Upon receiving a message with this object, Telegram clients will display a reply interface to the user (act as if the user has selected the bot‘s message and tapped ’Reply'). This can be extremely useful if you want to create user-friendly step-by-step interfaces without having to sacrifice privacy mode.
   | ForceReply
   {
