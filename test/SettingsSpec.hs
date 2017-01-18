c2c4765e8b50009e7679828a759271ef7a462128
Alexey Rodiontsev <ingar.ne@gmail.com>
2017-01-18 21:15:11 +0100

    added more complex test for management api

--- test/SettingsSpec.hs
+++ test/SettingsSpec.hs
@@ -5,6 +5,8 @@
 
 module SettingsSpec (spec) where
 
+import           Control.Concurrent
+import           Control.Monad.IO.Class
 import           Data.Text               (Text)
 import           Network.HTTP.Client     (newManager)
 import           Network.HTTP.Client.TLS (tlsManagerSettings)
@@ -14,12 +16,26 @@ import           Web.Telegram.API.Bot
 spec :: Token -> Text -> Spec
 spec token botName = do
   manager <- runIO $ newManager tlsManagerSettings
-  describe "/getMe and /getWebhookInfo" $
+  describe "/getMe and /getWebhookInfo work together" $
     it "responds with correct bot's name and empty webhook" $ do
-      res <- runClient (do
+      res <- runClient ( do
           b <- getMeM
           info <- getWebhookInfoM
           return (b, info)) token manager
       let Right (Response {result = me}, Response {result = whi}) = res
       user_first_name me `shouldBe` botName
+      whi_url whi `shouldBe` ""
+  describe "webhook operations" $
+    it "able to get and set webhook" $ do
+      res <- runClient ( do
+          info <- getWebhookInfoM
+          liftIO $ (whi_url . result) info `shouldBe` ""
+          liftIO $ threadDelay $ 2 * 1000 * 1000 -- to avoid Too many request error
+          set <- setWebhookM $ Just "https://example.com/bot"
+          liftIO $ result set `shouldBe` True
+          liftIO $ threadDelay $ 2 * 1000 * 1000 -- to avoid Too many request error
+          del <- deleteWebhookM
+          liftIO $ result del `shouldBe` True
+          getWebhookInfoM) token manager
+      let Right Response { result = whi } = res
       whi_url whi `shouldBe` ""
